083ceac72387d509cd4a52a61376d3869356c5c1
nippitch239 <48985884+nippitch239@users.noreply.github.com>
2023-11-01 21:06:12 +0700

    Add files via upload

---
+++ script/darktheme.js
@@ -0,0 +1,5 @@
+var X3 = document.getElementById("X3");
+
+X3.onclick = function(){
+    document.body.classList.toggle("dark-themes");
+}
---
+++ script/gen_validatorv31.js
@@ -0,0 +1,14 @@
+// <![CDATA[
+
+    var frmvalidator  = new Validator("contactus");
+    frmvalidator.EnableOnPageErrorDisplay();
+    frmvalidator.EnableMsgsTogether();
+    frmvalidator.addValidation("name","req","Please provide your name");
+
+    frmvalidator.addValidation("email","req","Please provide your email address");
+
+    frmvalidator.addValidation("email","email","Please provide a valid email address");
+
+    frmvalidator.addValidation("message","maxlen=2048","The message is too long!(more than 2KB!)");
+
+// ]]>
